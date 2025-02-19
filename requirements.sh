fa2d7dad1088f97389655d2a868bb79e5ad9dda5
Valtteri Huhdankoski <valtteri.huhdankoski@hotmail.com>
2025-02-19 15:11:16 +0200

    Add power control confs

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
-sudo apt install -y i3 libfuse2 git tmux kitty zsh clang gcc hsetroot picom rofi thunar ranger python-is-python3 xclip
+sudo apt install -y i3 libfuse2 git tmux kitty zsh clang gcc hsetroot picom rofi thunar ranger python-is-python3 xclip tlp
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 chsh -s $(which zsh)
 
@@ -6,6 +6,7 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # For laptops sudo nvim /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf change the setting to 2 for faster wifi
+# TLP is also for laptops, espcially thinkpads. Power control inependent of the charger connection. Can force discharge to conserve battey
 # FOSS projects:
 # https://github.com/zoltanp/xrandr-invert-colors
 # NVIM
