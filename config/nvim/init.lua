648a5307ea76d1ce21044fd604538269fa2eeb54
Valtteri Huhdankoski <valtteri.huhdankoski@hotmail.com>
2025-02-19 10:26:30 +0200

    Update req

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -82,7 +82,6 @@ I hope you enjoy your Neovim journey,
 
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
--- vim.api.nvim_set_option('clipboard', 'unnamed') Copy paste fix for work laptop
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
-sudo apt install -y i3 libfuse2 git tmux kitty zsh clang gcc hsetroot picom rofi thunar ranger
+sudo apt install -y i3 libfuse2 git tmux kitty zsh clang gcc hsetroot picom rofi thunar ranger python-is-python3 xclip
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 chsh -s $(which zsh)
 
