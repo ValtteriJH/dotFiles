03cd86f844de826e324a9566134c5da7220fd9a6
Valtteri Huhdankoski <valtteri.huhdankoski@hotmail.com>
2025-02-20 12:10:11 +0200

    add playerctl

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
-sudo apt install -y i3 libfuse2 git tmux kitty zsh clang gcc hsetroot picom rofi thunar ranger python-is-python3 xclip tlp
+sudo apt install -y i3 libfuse2 git tmux kitty zsh clang gcc hsetroot picom rofi thunar ranger python-is-python3 xclip tlp playerctl brightnessctl
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 chsh -s $(which zsh)
 
