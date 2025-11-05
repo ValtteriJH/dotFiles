0c365bedb095b5eb97779c22cc8ddacc89d0ffed
Valtteri <valtteri.j.huhdankoski@gmail.com>
2025-11-05 10:00:01 +0200

    Change default terminal to Alacritty, add alacritty confs and install scripts

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,11 @@
-sudo apt install -y i3 libfuse2 git tmux kitty zsh clang gcc hsetroot picom rofi thunar ranger python-is-python3 xclip tlp playerctl brightnessctl nodejs
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo apt install -y i3 libfuse2 git tmux kitty zsh clang gcc hsetroot picom rofi thunar ranger python-is-python3 xclip tlp playerctl brightnessctl nodejs cmake g++ pkg-config libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
+# DL alacritty
+cargo install alacritty
+# Alacritty proper install
+sudo cp ~/.cargo/bin/alacritty /usr/bin/
+
+sudo apt git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 chsh -s $(which zsh)
 
 # zsh plugins
@@ -10,6 +16,10 @@ git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
 
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+sudo cp ./Alacritty.desktop /usr/share/applications
+sudo cp ./settings.desktop /usr/share/applications
 
 # For laptops sudo nvim /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf change the setting to 2 for faster wifi
 # TLP is also for laptops, espcially thinkpads. Power control inependent of the charger connection. Can force discharge to conserve battey
